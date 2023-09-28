877013bbb02d7ec6d1a6b4bacc9fa8ed11ce863e
Adam Stankiewicz <stan.adamm@gmail.com>
2023-09-28 17:57:08 +0100

    Return frequencies of inputs

--- frequencies.py
+++ frequencies.py
@@ -4,4 +4,10 @@
 def frequencies(items):
     frequencies = {}
     # Your code goes here
+    for item in items:
+        item = str(item)
+        if item not in frequencies.keys():
+            frequencies.update({item : 1})
+        else:
+            frequencies.update({item : frequencies.get(item) + 1})
     return frequencies
